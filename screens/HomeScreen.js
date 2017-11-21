e5ad032c4d75226aa40219f101dcf2293e06a0dd
Ana Rodriguez <anarodriguez@Anas-MacBook-Air.local>
2017-11-21 12:28:44 +0100

    landing page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,15 +14,20 @@ import {
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
+import { Dimensions } from 'react-native';
+
 import { MonoText } from '../components/StyledText';
 import Colors from '../constants/Colors';
 
 import Carousel from 'react-native-snap-carousel';
 
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+
 const images = [
-  {image: require('../test1.jpeg'), text: 'Image 1 description'},
-  {image: require('../test2.jpeg'), text: 'Image 2 description'},
-  {image: require('../test3.jpeg'), text: 'Image 3 description'}
+  {image: require('../test1.jpeg'), text: '12 weeks to your first Sprint Tri'},
+  {image: require('../test2.jpeg'), text: '12 week Personalized Training Plan'},
+  {image: require('../test3.jpeg'), text: 'Get Fit, Confident, and RACE READY!'}
 ]
 
 export default class HomeScreen extends React.Component {
@@ -62,25 +67,27 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
+          <View style={styles.borderTop} >
+          </View>
+          <View style={styles.strip} >
+          </View>
+          <View>
             <Image
               source={require('../tri.png')}
-              style={styles.welcomeImage}
+              style={styles.logo}
             />
           </View>
-
-          <View style={styles.getStartedContainer}>
+          <View>
             <Text style={styles.titleText}>GO-TRI-GO</Text>
           </View>
-
           <Carousel
               renderItem={this._renderItem}
               data={images}
-              sliderWidth={250}
-              itemWidth={160}
-            />
-
+              sliderWidth={320}
+              itemWidth={215}
+              loop={true}
+              activeSlideAlignment={'center'}
+          />
           <View style={styles.LoginRegisterTextContainer}>
             <TouchableOpacity
               onPress={() => {this.goToLogin()}}>
@@ -91,15 +98,6 @@ export default class HomeScreen extends React.Component {
               <Text style={styles.LoginRegisterText}>Register</Text>
             </TouchableOpacity>
           </View>
-
-        </ScrollView>
-
-        {/* <View style={styles.tabBarInfoContainer}> */}
-
-          {/* <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View> */}
-        {/* </View> */}
       </View>
     );
   }
@@ -109,64 +107,119 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.ourGrey,
-  },
-  contentContainer: {
-    paddingTop: 20,
+    paddingTop: 10,
     alignItems: 'center'
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 5,
-    marginBottom: 10,
-  },
-  welcomeImage: {
+  logo: {
     width: 140,
-    height: 120,
+    height: 52,
     resizeMode: 'contain',
-    marginTop: 0,
-    marginLeft: -10,
+    marginTop: 40,
+    marginBottom: 5
   },
   titleText: {
     fontFamily: 'kalam-bold',
     fontSize: 44,
     color: Colors.ourBlue,
-    lineHeight: 56,
     textAlign: 'center',
-  },
-  subtitleText: {
-    fontFamily: 'kalam-bold',
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
+    backgroundColor: 'transparent'
   },
   LoginRegisterTextContainer: {
-    marginTop: 50,
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: 280
+    width: 230,
+    paddingBottom: 15
   },
   LoginRegisterText: {
-    fontSize: 30,
+    fontSize: 20,
     color: Colors.ourGreen,
+    fontFamily: 'kalam-bold',
     textAlign: 'center',
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: 'transparent'
   },
   slideContainer: {
     marginTop: 20,
     flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
+    width: 215,
   },
   slideImage: {
     height: 284.44,
-    width: 160
+    width: 160,
   },
   slideText: {
     textAlign: 'center',
     fontSize: 20,
-    color: Colors.ourOrange,
-    marginTop: 14
+    color: 'white',
+    fontStyle: 'italic',
+    marginTop: 14,
+    backgroundColor: 'transparent'
+  },
+  triangle: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderRightWidth: screenWidth+50,
+    borderTopWidth: screenHeight+80,
+    borderRightColor: '#5e5d5e',
+    borderTopColor: 'transparent',
+    position: 'absolute',
+    zIndex: -2
+  },
+  borderTop: {
+    flex: 1,
+    height: screenHeight-50,
+    width: screenWidth-30,
+    marginTop: 30,
+    marginBottom: 20,
+    marginLeft: 15,
+    marginRight: 15,
+    borderTopColor: Colors.ourBlue,
+    borderBottomColor: Colors.ourBlue,
+    borderRightColor: 'transparent',
+    borderLeftColor: 'transparent',
+    borderWidth: 1,
+    borderRadius: 15,
+    position: 'absolute',
+    zIndex: -1
+  },
+  borderSide: {
+    flex: 1,
+    height: screenHeight-50,
+    width: screenWidth-30,
+    marginTop: 30,
+    marginBottom: 20,
+    marginLeft: 15,
+    marginRight: 15,
+    borderTopColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderRightColor: 'white',
+    borderLeftColor: 'white',
+    borderWidth: 1,
+    borderRadius: 15,
+    position: 'absolute',
+    zIndex: -1
+  },
+  strip: {
+    flex: 1,
+    height: 375,
+    width: 800,
+    backgroundColor: '#5e5d5e',
+    position: 'absolute',
+    zIndex: -1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 180,
+    borderWidth: 1,
+    borderColor: 'white',
+    transform: [
+      {rotate: '-10deg'}
+    ]
   }
 });
